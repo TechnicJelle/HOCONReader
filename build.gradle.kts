e4bbd1aed8ccebfda26f691cb171e83738a6a0b6
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2026-01-13 13:54:37 +0100

    Include git repo link and git hash in the jar manifest

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import java.io.IOException
+import java.util.concurrent.TimeoutException
+
 plugins {
 	java
 	id("com.gradleup.shadow") version "9.3.1"
@@ -20,5 +23,40 @@ dependencies {
 tasks.jar {
 	manifest {
 		attributes["Main-Class"] = "com.technicjelle.HOCONReader"
+		attributes["git-repo"] = "https://github.com/TechnicJelle/HOCONReader"
+		attributes["git-hash"] = gitHash() + if (!gitClean()) "-dirty" else ""
 	}
+}
+
+// The following three functions are copied from BlueMap's source code, under the MIT Licence
+// https://github.com/BlueMap-Minecraft/BlueMap/blob/6c59ab44e506028a99efe1a49eab4531425971ad/buildSrc/src/main/kotlin/versioning.kt
+
+fun Project.gitHash(): String {
+	return runCommand("git rev-parse --verify HEAD", "-")
+}
+
+fun Project.gitClean(): Boolean {
+	if (runCommand("git update-index --refresh", "NOT-CLEAN") == "NOT-CLEAN") return false
+	return runCommand("git diff-index HEAD --", "NOT-CLEAN").isEmpty()
+}
+
+private fun Project.runCommand(cmd: String, fallback: String? = null): String {
+	ProcessBuilder(cmd.split("\\s(?=(?:[^'\"`]*(['\"`])[^'\"`]*\\1)*[^'\"`]*$)".toRegex()))
+		.directory(projectDir)
+		.redirectOutput(ProcessBuilder.Redirect.PIPE)
+		.redirectError(ProcessBuilder.Redirect.PIPE)
+		.start()
+		.apply {
+			if (!waitFor(10, TimeUnit.SECONDS))
+				throw TimeoutException("Failed to execute command: '$cmd'")
+		}
+		.run {
+			val exitCode = waitFor()
+			if (exitCode == 0) return inputStream.bufferedReader().readText().trim()
+
+			val error = errorStream.bufferedReader().readText().trim()
+			logger.warn("Failed to execute command '$cmd': $error")
+			if (fallback != null) return fallback
+			throw IOException(error)
+		}
 }
